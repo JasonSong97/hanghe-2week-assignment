e3061a78aae1f18f99008fbed106a8a315d7245c
JasonSong97 <jaegeunsong97@gmail.com>
2024-12-22 13:53:28 +0900

    first commit

---
+++ src/main/java/com/example/hanghe_2week_assignment/Hanghe2weekAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghe_2week_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hanghe2weekAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hanghe2weekAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hanghe_2week_assignment/Hanghe2weekAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanghe_2week_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hanghe2weekAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
